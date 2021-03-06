598ca39543af928bc3d5a2a8966269f71ac088c1
Diego Ruelas <diego.ruelas@live.com>
2021-03-05 19:05:03 -0600

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 
 // Need to update below config
 const config = {
-    apiKey: "AIzaSyDQDPsbDv5uCMe5Dj-sl_iakmtoRkfKNS0",
-    authDomain: "salvadors-hotel.firebaseapp.com",
-    projectId: "salvadors-hotel",
-    storageBucket: "salvadors-hotel.appspot.com",
-    messagingSenderId: "204879550163",
-    appId: "1:204879550163:web:68855cb5f9323e88f8a626",
-    measurementId: "G-5G3WYX3424"
+   apiKey: "AIzaSyBLvUV6_LzCgzwC8mMdL9qd73w5L72qiJI",
+    authDomain: "fidelio-3cd32.firebaseapp.com",
+    projectId: "fidelio-3cd32",
+    storageBucket: "fidelio-3cd32.appspot.com",
+    messagingSenderId: "1094375557869",
+    appId: "1:1094375557869:web:e5c767a53f004926145ae4",
+    measurementId: "G-TEEVRM7R90"
 };
 
 if (!firebase.apps.length) {
